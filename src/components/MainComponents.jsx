3e1175fe93febfbc7b4de0c59e048d55d0db1280
Jesus Rosales <jrosales6213@gmail.com>
2021-10-18 15:25:13 -0700

    added sidebar

---
+++ src/components/MainComponents.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Example from './NavbarComponent';
+// import './App.css';
+import SideBar from './SideBar';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+function Main(){
+    return (
+      <div>
+      <Example />
+      <SideBar/>
+    </div>
+    )
+  }
+
+
+export default Main;
