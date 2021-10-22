e4ee17dda53b67ecdb8bf51ec65892a448cf8ad1
Jesus Rosales <jrosales6213@gmail.com>
2021-10-21 21:56:06 -0700

    added new sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
-import Main from './components/MainComponents';
+import React, { useState } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import SideBar from "./components/sidebar/SideBar.js";
+import Content from "./components/content/Content";
+import "./App.css";
 
-function App(){
-    return (
-      <Main />
-    )
-  }
+const App = () => {
+  const [sidebarIsOpen, setSidebarOpen] = useState(true);
+  const toggleSidebar = () => setSidebarOpen(!sidebarIsOpen);
 
+  return (
+    <Router>
+      <div className="App wrapper">
+        <SideBar className=".text-white" toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+        <Content toggleSidebar={toggleSidebar} sidebarIsOpen={sidebarIsOpen} />
+      </div>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/components/sidebar/SideBar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faPlusCircle,
+  faWrench,
+  faHistory,
+  faChartBar,
+  faCopy
+} from "@fortawesome/free-solid-svg-icons";
+import { NavItem, NavLink, Nav } from "reactstrap";
+import classNames from "classnames";
+import { Link } from "react-router-dom";
+
+
+import SubMenu from "./SubMenu";
+
+const SideBar = ({ isOpen, toggle }) => (
+  <div className={classNames("sidebar", { "is-open": isOpen })}>
+    <div className="sidebar-header">
+      <span color="info" onClick={toggle} style={{ color: "#fff" }}>
+        &times;
+      </span>
+      <h3>Auto React</h3>
+    </div>
+    <div className="side-menu">
+      <Nav vertical className= "list-unstyled pb-3">
+        <p>Main Page</p>
+        <SubMenu  title="Home" icon={faHome} items={submenus[0]} />
+        <NavItem>
+          <NavLink tag={Link} to={"/about"}>
+            <FontAwesomeIcon className="mr-2" icon={faPlusCircle} />
+             Add Car
+          </NavLink>
+        </NavItem>
+        <SubMenu title=" Pages" icon={faCopy} items={submenus[1]} />
+        <NavItem>
+          <NavLink tag={Link} to={"/pages"}>
+            <FontAwesomeIcon icon={faWrench} className="mr-2" />
+          Service
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to={"/faq"}>
+            <FontAwesomeIcon icon={faHistory} className="mr-2" />
+            History
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={Link} to={"/contact"}>
+            <FontAwesomeIcon icon={faChartBar} className="mr-2" />
+            Stats
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </div>
+  </div>
+);
+
+const submenus = [
+  [
+    {
+      title: "Honda Civic",
+      target: "Home-1",
+    },
+    {
+      title: "Toyota Corolla",
+      target: "Home-2",
+    },
+    {
+      itle: "Home 3",
+      target: "Home-3",
+    },
+  ],
+  [
+    {
+      title: "Page 1",
+      target: "Page-1",
+    },
+    {
+      title: "Page 2",
+      target: "Page-2",
+    },
+  ],
+];
+
+export default SideBar;
