d252622455daed69ab2b763379f739936aacc5fd
Jesus Rosales <jrosales6213@gmail.com>
2021-11-03 17:50:48 -0700

    added a list of carBrands and models to modal

--- src/components/sidebar/ModalForm.js
+++ src/components/sidebar/ModalForm.js
@@ -1,9 +1,10 @@
 import React, {useState} from "react";
 import { Button, Modal, ModalHeader, ModalBody,Label, Form, FormControl} from 'reactstrap';
 import classNames from "classnames";
+import carList from "../../shared/carInfo";
 
 
-const ModalForm = (props) => {
+const ModalForm = () => {
   const [collapsed, setCollapsed] = useState(true);
   const [name, setName] = useState('');
   const [model, setModel] = useState('');
@@ -15,31 +16,10 @@ const ModalForm = (props) => {
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    alert(name + model + year + mileage)
+    alert("Your " + year +" "+ name +" "+ model +" "+ "was added to the list")
     console.log(name + model + year + mileage)
-    
-   
   }
   
-  const models = [
-    {
-      label: "Civic",
-      value: "Civic",
-    },
-    {
-      label: "Accord",
-      value: "Accord",
-    },
-    {
-      label: "Prius",
-      value: "Prius",
-    },
-    {
-      label: "Tacoma",
-      value: "Tacoma",
-    },
-  ];
-
 
   return (
     <div>
@@ -58,27 +38,25 @@ const ModalForm = (props) => {
                           <Form onSubmit={handleSubmit} >
                               <div className="form-group">
                                       <Label>Make</Label>
-                                      <input  value={name} onChange={e => setName(e.target.value)} className="form-control" />
+                                      <select value={name} onChange={e => setName(e.target.value)} className="form-control" >
+                                      {carList.map((carlist) => (
+                                      <option value={carlist.value}>{carlist.brand}</option>
+                                       ))}
+                                      </select>
                                        
                               </div>
                               <div className="form-group">
-                                      <Label>Model</Label>
-                                      <select value={model} onChange={e => setModel(e.target.value)} className="form-control" >
-                                      {models.map((model) => (
-                                      <option value={model.value}>{model.label}</option>
+                                      <Label>Model</Label>                                      
+                                       <select value={model} onChange={e => setModel(e.target.value)} className="form-control" >
+                                      {carList.map((carlist) => (
+                                      <option value={carlist.value}>{carlist.models[0]}</option>
                                        ))}
-                                      </select>
+                                      </select> 
                                       
                               </div>
                               <div className="form-group">
                                   <Label >Year</Label>
-                                  <select value={year} onChange={e => setYear(e.target.value)} className="form-control" >
-                                          <option>2016</option>
-                                          <option>2017</option>
-                                          <option>2018</option>
-                                          <option>2019</option>
-                                          <option>2020</option>
-                                      </select>
+                                  <input value={year} onChange={e => setYear(e.target.value)} className="form-control" />          
                               </div>
                               <div className="form-group">
                                   <Label >Mileage</Label>
@@ -99,4 +77,8 @@ export default ModalForm;
 // 11-2-21 - I switched up the form to a different style of using hooks. I set hooks for make , model , year and mileage. 
 //           Also learned to map through the array of cars and instead of using hard coded car options, i just maped through the array.   
 //           I want to continue to map through the other properties, then I can start on logging the information from the modal form. 
+
+// 11-3-21 - Pulled a list of car brands and models from github (https://github.com/matthlavacka/car-list/blob/master/car-list.json) and used it as a sample API i would like to 
+//           implement later on. The things I still have to work on the modal is making sure the Brand & models match. Also need to get the data from the response and show it the server
+//           and render it on the HOme page.  
   
