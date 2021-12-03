082f51dc26844bdabec6476240f7ab381a3c35b7
Jesus Rosales <jrosales6213@gmail.com>
2021-12-02 22:09:21 -0800

    debugging redux

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,23 +1,38 @@
-;import { actionTypes } from 'react-redux-form';
+// ;import { actionTypes } from 'react-redux-form';
+// import { COMMENTS } from '../shared/comments';
+// import * from './ActionTypes';
+
+
+// export default Comments =  (state = COMMENTS, action) => {
+//   switch (action.type) {
+//     case actionTypes.ADD_COMMENT:
+//       const comment = action.payload;
+//             comment.id = state.length;
+//             comment.date = new Date().toLocaleDateString();
+//             return state.concat(comment);
+//             break;
+//     case actionTypes.DELETE_COMMENT:
+//            return state.filter(comment => comment !== action.payload);    
+//             break;
+//       case actionTypes.EDIT_COMMENT:
+//            console.log(EDIT_COMMENT) 
+//             break; 
+//           default:
+//               return state;
+//   }
+// }
 import { COMMENTS } from '../shared/comments';
-import * from './ActionTypes';
+import * as ActionTypes from './ActionTypes';
 
-
-export default Comments =  (state = COMMENTS, action) => {
-  switch (action.type) {
-    case actionTypes.ADD_COMMENT:
-      const comment = action.payload;
+export const Comments = (state = COMMENTS, action) => {
+    switch (action.type) {
+        case ActionTypes.ADD_COMMENT:
+            const comment = action.payload;
             comment.id = state.length;
             comment.date = new Date().toLocaleDateString();
+            //Not sure is Date will cause buggs with date action Creator.//
             return state.concat(comment);
-            break;
-    case actionTypes.DELETE_COMMENT:
-           return state.filter(comment => comment !== action.payload);    
-            break;
-      case actionTypes.EDIT_COMMENT:
-           console.log(EDIT_COMMENT) 
-            break; 
-          default:
-              return state;
-  }
-}
+        default:
+            return state;
+    }
+};
