70eead1adaf821deadf7fafc67818b2c9f87e7b3
Jesus Rosales <jrosales6213@gmail.com>
2021-12-09 06:41:15 -0800

    modified topbar/icon/notification

--- src/components/content/Topbar.js
+++ src/components/content/Topbar.js
@@ -5,76 +5,19 @@ import {
   faBell,
   faCog,
   faSignOutAlt,
-  faBellO,
-  faConciergeBell,
-  faBellSlash,
 } from "@fortawesome/free-solid-svg-icons";
 import { faAlignJustify } from "@fortawesome/free-solid-svg-icons";
 import {
   Navbar,
   Button,
-  NavbarToggler,
-  Collapse,
   Nav,
-  NavItem,
-  NavLink,
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
   UncontrolledDropdown,
-  Dropdown,
-  Alert,
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { render } from "@testing-library/react";
-
-function AlertIcon() {
-  return (
-    <li className="notifications new">
-      <a href="" data-toggle="dropdown">
-        <i className="fa fa-bell-o"></i>
-        <sup>
-          <span className="counter">2</span>
-        </sup>
-      </a>
-
-      <DropdownMenu className="notifications-dropdown-menu">
-        <ul className="notifications-container">
-          <li>
-            <a href="" className="notification-item">
-              <div class="body-col">
-                <p>
-                  <span className="accent">Toyota</span> New Recall:
-                  <span className="accent">
-                    Problem with Air Conditioner System
-                  </span>
-                  .
-                </p>
-              </div>
-            </a>
-          </li>
-          <li>
-            <a href="" className="notification-item">
-              <div class="body-col">
-                <p>
-                  <span className="accent">Toyota</span> Upcoming maintenance:
-                  <span className="accent">12/21/22</span>.
-                </p>
-              </div>
-            </a>
-          </li>
-        </ul>
-        <footer>
-          <ul>
-            <li>
-              <a href="">View All</a>
-            </li>
-          </ul>
-        </footer>
-      </DropdownMenu>
-    </li>
-  );
-}
 
 class Topbar extends Component {
   constructor(props) {
@@ -96,137 +39,78 @@ class Topbar extends Component {
         color="light"
         light
         className="navbar shadow-sm p-3 mb-5 bg-light "
-        expand="md"
+        expand="xs"
       >
         <Button color="info" onClick={this.props.onMenuToggle}>
           <FontAwesomeIcon icon={faAlignJustify} />
         </Button>
-        {/* <NavbarToggler onClick={this.toggleTopBar} /> */}
-        {/* <DropdownMenu className="notifications-dropdown-menu"></DropdownMenu> */}
 
-        {/* <Collapse isOpen={this.state.topBarIsOpen} navbar> */}
         <Nav className="ml-auto" navbar>
-          {/* <NavItem>
-            <NavLink tag={Link} to={"/page-1"}>
-              page 1
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to={"/page-2"}>
-              page 2 {AlertIcon}
-            </NavLink>
-          </NavItem> */}
+          {/*---- NOTIFICAITON ICON------*/}
           <UncontrolledDropdown inNavbar nav>
-            <DropdownToggle caret nav>
-              <a href="" data-toggle="dropdown">
-                <FontAwesomeIcon icon={faBell} size="lg" />
-                <sup>
-                  <span className="counter">2</span>
-                </sup>
-              </a>
+            <DropdownToggle nav className="notifications new p-2">
+              {/* <a href="" data-toggle="dropdown"> */}
+              <FontAwesomeIcon icon={faBell} size="lg" />
+              <sup>
+                <span className="counter">2</span>
+              </sup>
+              {/* </a> */}
             </DropdownToggle>
-            <DropdownMenu right className="notifications-dropdown-menu">
+
+            {/* ----NOTIFICATION TOGGLE----- */}
+            <DropdownMenu className="notifications-dropdown-menu">
               <DropdownItem>
-                <FontAwesomeIcon icon={faUser} className="mr-1" />
-                Profile
+                <div className="body-col">
+                  <p>
+                    <span className="accent">Recall: </span>
+                    Electrical problems in the interior of vehicle.
+                  </p>
+                </div>
               </DropdownItem>
               <DropdownItem>
-                <FontAwesomeIcon icon={faBell} className="mr-1" />
-                Notifications
-              </DropdownItem>
-              <DropdownItem>
-                <FontAwesomeIcon icon={faCog} className="mr-1" />
-                Settings
-              </DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>
-                <FontAwesomeIcon icon={faSignOutAlt} className="mr-1" />
-                Log Out
+                <div className="body-col">
+                  <p>
+                    <span className="accent">Reminder: </span>
+                    Oil change in 2 days.
+                  </p>
+                </div>
               </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
 
-          {/* <NavItem>
-            <li className="notifications new">
-              <a
-                href=""
-                data-toggle="dropdown"
-                className="align-content-center"
-              >
-                <FontAwesomeIcon icon={faBell} size="lg" />
-                <sup>
-                  <span className="counter">2</span>
-                </sup>
-              </a>
-              <DropdownMenu className="notifications-dropdown-menu">
-                <ul className="notifications-container">
-                  <li>
-                    <a href="" className="notification-item">
-                      <div className="body-col">
-                        <p>
-                          <span className="accent">Toyota</span> New Recall:
-                          <span className="accent">
-                            Problem with Air Conditioner System
-                          </span>
-                          .
-                        </p>
-                      </div>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="" className="notification-item">
-                      <div class="body-col">
-                        <p>
-                          <span className="accent">Toyota</span> Upcoming
-                          maintenance:
-                          <span className="accent">12/21/22</span>.
-                        </p>
-                      </div>
-                    </a>
-                  </li>
-                </ul>
-                <footer>
-                  <ul>
-                    <li>
-                      <a href="">View All</a>
-                    </li>
-                  </ul>
-                </footer>
-              </DropdownMenu>
-            </li>
-          </NavItem> */}
-
+          {/*---- PROFILE ICON -----*/}
           <UncontrolledDropdown inNavbar nav>
-            <DropdownToggle caret nav>
+            <DropdownToggle nav>
               <img
                 className="avatar-img"
                 src="https://www.w3schools.com/howto/img_avatar.png"
                 alt="avatar"
               />
-              <span class="name">Jesus Rosales </span>
+              <span class="name">JR</span>
             </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>
+
+            {/* PROFILE ICON TOGGLE */}
+            <DropdownMenu className="profile-dropdown-menu" right>
+              <DropdownItem className="profile-dropdown-item">
                 <FontAwesomeIcon icon={faUser} className="mr-1" />
                 Profile
               </DropdownItem>
-              <DropdownItem>
+              <DropdownItem className="profile-dropdown-item">
                 <FontAwesomeIcon icon={faBell} className="mr-1" />
                 Notifications
               </DropdownItem>
-              <DropdownItem>
+              <DropdownItem className="profile-dropdown-item">
                 <FontAwesomeIcon icon={faCog} className="mr-1" />
                 Settings
               </DropdownItem>
               <DropdownItem divider />
-              <DropdownItem>
+              <DropdownItem className="profile-dropdown-item">
                 <FontAwesomeIcon icon={faSignOutAlt} className="mr-1" />
                 Log Out
               </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
-        {/* </Collapse> */}
       </Navbar>
     );
   }
